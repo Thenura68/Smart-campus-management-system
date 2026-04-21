02bd9e3b87bdb224d1e5aca90052105d54e6618c
Manith-Edirisinghe <manith.edirisinghe@icloud.com>
2026-04-21 09:11:21 +0530

    comment view for technician implemented
    comment view for technician implemented and delete ticket error fixed

--- backend/smart-campus-api/src/main/java/com/smartcampus/ticket/repository/CommentRepository.java
+++ backend/smart-campus-api/src/main/java/com/smartcampus/ticket/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package com.smartcampus.ticket.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.smartcampus.ticket.model.Comment;
 
@@ -15,6 +17,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
     //To delete the comment along with a ticket delete
+    @Transactional
+    @Modifying
     void deleteByTicketId(Long ticketId);
 
 }
